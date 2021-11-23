842bf01fe08508562d1f42163cbbc376da3cd274
Todd Nordquist <toddnordquist144@gmail.com>
2021-11-23 14:54:49 -0700

    Add emojis.

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker14/service/SettingsRepository.java
@@ -0,0 +1,54 @@
+package edu.cnm.deepdive.codebreaker14.service;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.res.Resources;
+import androidx.preference.PreferenceManager;
+import edu.cnm.deepdive.codebreaker14.R;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+
+public class SettingsRepository {
+
+  private final SharedPreferences preferences;
+  private final String codeLengthPrefKey;
+  private final String poolSizePrefKey;
+  private final int codeLengthPrefDefault;
+  private final int poolSizePrefDefault;
+  private final OnSharedPreferenceChangeListener listener;
+
+  private ObservableEmitter<Integer> codeLengthPrefEmitter;
+  private ObservableEmitter<Integer> poolSizePrefEmitter;
+
+  public SettingsRepository(Context context) {
+    Resources resources = context.getResources();
+    codeLengthPrefKey = resources.getString(R.string.code_length_pref_key);
+    poolSizePrefKey = resources.getString(R.string.pool_size_pref_key);
+    codeLengthPrefDefault = resources.getInteger(R.integer.code_length_pref_default);
+    poolSizePrefDefault = resources.getInteger(R.integer.pool_size_pref_default);
+    preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    listener = this::emitChangedPreferences;
+    preferences.registerOnSharedPreferenceChangeListener(listener);
+  }
+
+  public Observable<Integer>  getCodeLengthPreferences() {
+    return Observable.create((emitter) -> {
+      codeLengthPrefEmitter = emitter;
+      emitChangedPreferences(preferences, codeLengthPrefKey);
+    });
+  }
+
+  private void emitChangedPreferences(SharedPreferences prefs, String key) {
+    if (key.equals(codeLengthPrefKey)) {
+      if (codeLengthPrefEmitter != null && !codeLengthPrefEmitter.isDisposed()) {
+        codeLengthPrefEmitter.onNext(prefs.getInt(key, codeLengthPrefDefault));
+      }
+    } else if (key.equals(poolSizePrefKey)) {
+      if (poolSizePrefEmitter != null && !poolSizePrefEmitter.isDisposed()) {
+        poolSizePrefEmitter.onNext(prefs.getInt(key, poolSizePrefDefault));
+      }
+    }
+  }
+
+}
--- app/src/main/java/edu/cnm/deepdive/codebreaker14/viewmodel/PlayViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker14/viewmodel/PlayViewModel.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.codebreaker14.viewmodel;
 
 import android.app.Application;
+import android.media.audiofx.BassBoost.Settings;
 import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -9,25 +10,35 @@ import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.OnLifecycleEvent;
+import edu.cnm.deepdive.codebreaker14.R;
 import edu.cnm.deepdive.codebreaker14.model.entity.Game;
 import edu.cnm.deepdive.codebreaker14.model.entity.Guess;
 import edu.cnm.deepdive.codebreaker14.service.GameRepository;
+import edu.cnm.deepdive.codebreaker14.service.SettingsRepository;
 import io.reactivex.disposables.CompositeDisposable;
 
 public class PlayViewModel extends AndroidViewModel implements LifecycleObserver {
 
-  GameRepository repository;
+  private final GameRepository gameRepository;
+  private final SettingsRepository settingsRepository;
   private final MutableLiveData<Game> game;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
 
+  private int codeLength;
+  private int poolSize;
+  private String basePool;
+
   public PlayViewModel(@NonNull Application application) {
     super(application);
-    repository = new GameRepository();
+    gameRepository = new GameRepository();
+    settingsRepository = new SettingsRepository(application);
     game = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
-    startGame();
+    String[] emojis = application.getResources().getStringArray(R.array.emojis);
+    basePool = String.join("", emojis);
+    subscribeToSettings();
   }
 
   public LiveData<Game> getGame() {
@@ -39,18 +50,24 @@ public class PlayViewModel extends AndroidViewModel implements LifecycleObserver
   }
 
   public void startGame() {
-    throwable.postValue(null);
-    Game game = new Game();
-    game.setPool("ABCDEF"); // TODO Read value from shared preferences
-    game.setLength(3); // TODO Read value from shared preferences
-    pending.add(
-        repository
-            .save(game)
-            .subscribe(
-                this.game::postValue,
-                this::postThrowable
-            )
-    );
+    if (codeLength > 0 && poolSize > 0) {
+      throwable.postValue(null);
+      int[] poolCodePoints = basePool
+          .codePoints()
+          .limit(poolSize)
+          .toArray();
+      Game game = new Game();
+      game.setPool(new String(poolCodePoints, 0, poolCodePoints.length));
+      game.setLength(codeLength);
+      pending.add(
+          gameRepository
+              .save(game)
+              .subscribe(
+                  this.game::postValue,
+                  this::postThrowable
+              )
+      );
+    }
   }
 
   public void submitGuess(String text) {
@@ -58,10 +75,35 @@ public class PlayViewModel extends AndroidViewModel implements LifecycleObserver
     Guess guess = new Guess();
     guess.setText(text);
     pending.add(
-        repository
+        gameRepository
             .save(game.getValue(), guess)
             .subscribe(
                 game::postValue,
+                this::postThrowable
+            )
+    );
+  }
+
+  private void subscribeToSettings() {
+    pending.add(
+        settingsRepository
+            .getCodeLengthPreferences()
+            .subscribe(
+                (codeLength) -> {
+                  this.codeLength = codeLength;
+                  startGame();
+                },
+                this::postThrowable
+            )
+    );
+    pending.add(
+        settingsRepository
+            .getCodeLengthPreferences()
+            .subscribe(
+                (poolSize) -> {
+                  this.poolSize = poolSize;
+                  startGame();
+                },
                 this::postThrowable
             )
     );
